a6814e5f506331255ed649c67f95a77e6e8001b4
RamonMamon <35195972+RamonMamon@users.noreply.github.com>
2019-06-20 13:11:08 +0800

    Implemented some Step 9 of the starting steps stated in the facebook documentation.

--- app/src/main/java/com/example/dreamstream/MainActivity.java
+++ app/src/main/java/com/example/dreamstream/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.dreamstream;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity
 {
 
     Switch joinNetworkSwitch;
+    CallbackManager callbackManager;
+    private static final String EMAIL = "email";
+    LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,7 +39,7 @@ public class MainActivity extends AppCompatActivity
                 {
                     // Join the Dream Network
 
-                    // Connect Facebook account
+                    // Connect Facebook account (If not already connected)
 
                     // Send Friend request on Facebook
 
@@ -46,6 +58,34 @@ public class MainActivity extends AppCompatActivity
                     System.out.println("Unchecking");
 
                 }
+            }
+        });
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                // login result contains the access token.
+                System.out.println("Login Successful");
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                System.out.println("Login Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                System.out.println("Login Failed");
             }
         });
 
