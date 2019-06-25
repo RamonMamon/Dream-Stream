e5113fd642dd9ff1df503f963243250a92ceb217
RamonMamon <35195972+RamonMamon@users.noreply.github.com>
2019-06-25 17:51:36 +0800

    Implemented working login button and switch state checker. Need to find a way to send friend requests on Facebook after the user has connected to the network.

--- app/src/main/java/com/example/dreamstream/MainActivity.java
+++ app/src/main/java/com/example/dreamstream/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.dreamstream;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -8,6 +9,7 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -20,19 +22,22 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity
 {
 
-    Switch joinNetworkSwitch;
-    CallbackManager callbackManager;
+    private Switch joinNetworkSwitch;
+    private CallbackManager callbackManager;
     private static final String EMAIL = "email";
-    LoginButton loginButton;
-    boolean isLoggedIn;
+    private LoginButton loginButton;
+    private boolean isLoggedIn;
+    private AccessToken accessToken;
+    private String PREF_NAME = "switch1_state";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        accessToken = AccessToken.getCurrentAccessToken();
+        joinNetworkSwitch = findViewById(R.id.switch1);
 
-        AccessToken accessToken = AccessToken.getCurrentAccessToken();
         try
         {
             isLoggedIn = accessToken != null || !accessToken.isExpired();
@@ -42,8 +47,11 @@ public class MainActivity extends AppCompatActivity
             e.printStackTrace();
         }
 
+        SharedPreferences settings = getSharedPreferences(PREF_NAME, 0);
+        boolean silent = settings.getBoolean("switchkey", false);
 
-        joinNetworkSwitch = findViewById(R.id.switch1);
+        // Only keeps the switch on if the user is logged in.
+        joinNetworkSwitch.setChecked(isLoggedIn && silent);
 
         joinNetworkSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
         {
@@ -52,42 +60,40 @@ public class MainActivity extends AppCompatActivity
             {
                 if (isChecked)
                 {
-                    // Join the Dream Network
-//                    System.out.println("Access Token: " + accessToken.getToken());
+                    System.out.println("Checking");
 
                     // Connect Facebook account (If not already connected)
                     if(!isLoggedIn)
                     {
                         // Popup that the user should connect their facebook account first.
 
-                        //
                         System.out.println("Please connect Facebook account first prior to connecting to the network.");
 
                         //
                         joinNetworkSwitch.setChecked(false);
                     }else
                     {
+                        System.out.println("Connected to the Dream network.");
+                        System.out.println("Access Token: " + accessToken.getToken());
                         // Send friend request on facebook
 
                         // Listen out for any Facebook Live streams.
-
-                        System.out.println("Connected to the Dream network.");
 
                     }
 
                     // Follow this process to watch the live streams of users on the dream network.
 
-                    System.out.println("Checking");
-
                 }else
                 {
-                    // Leave the dream network.
+                    System.out.println("Unchecking");
 
                     // Remove Dream network from list of friends.
 
-                    System.out.println("Unchecking");
-
                 }
+                SharedPreferences settings = getSharedPreferences(PREF_NAME, 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean("switchkey", isChecked);
+                editor.commit();
             }
         });
 
@@ -103,8 +109,10 @@ public class MainActivity extends AppCompatActivity
             public void onSuccess(LoginResult loginResult) {
                 // App code
                 // login result contains the access token.
+                accessToken = loginResult.getAccessToken();
+                isLoggedIn = true;
                 System.out.println("Login Successful");
-                System.out.println("Access key:" + loginResult.getAccessToken().getToken());
+                System.out.println("Access key:" + accessToken.getToken());
             }
 
             @Override
@@ -120,7 +128,25 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        // Access Token Listener
+        AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(
+                    AccessToken oldAccessToken,
+                    AccessToken currentAccessToken) {
 
+                if (currentAccessToken == null){
+                    //User logged out
+                    isLoggedIn = false;
+                    accessToken = null;
+                }
+                else
+                {
+                    accessToken = currentAccessToken;
+                    isLoggedIn = true;
+                }
+            }
+        };
     }
 
     @Override
